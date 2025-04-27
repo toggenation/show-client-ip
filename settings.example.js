0d7de508f91fb5d4b03e01d45e6e3b51a5cebbff
James McDonald <james@toggen.com.au>
2025-04-27 05:43:03 +0000

    Updated setting.example.js

--- settings.example.js
+++ settings.example.js
@@ -1,5 +1,5 @@
 // copy this to settings.js
 const settings = {
-		server1: "https://sitea.example.com.au",
-		server2: "https://siteb.example.com.au"
+		server1: "https://sitea.example.com/ip.php",
+		server2: "https://siteb.example.com/ip.php"
 }
