db5990f6788732eb89ac91734ebe373b0eacb17e
James McDonald <james@toggen.com.au>
2025-04-27 05:26:11 +0000

    Add settings.js

--- config.example.php
+++ config.example.php
@@ -1,4 +1,4 @@
 <?php 
 
-
+// copy this to config.php and modify to suit
 $acceptedOrigins = [ 'https://sitea.example.com.au', 'https://siteb.example.com.au', 'https://example.com.au' ];
---
+++ settings.example.js
@@ -0,0 +1,5 @@
+// copy this to settings.js
+const settings = {
+		server1: "https://sitea.example.com.au",
+		server2: "https://siteb.example.com.au"
+}
